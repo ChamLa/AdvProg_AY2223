4c5f147cea5a941ca5e93f0971a3b1c81ce6d710
ChamLa <22021125@vnu.edu.vn>
2023-03-24 11:19:48 +0700

    l3&l4&l6&l7

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -17,7 +17,7 @@ using std::cin;
 int generateRandomNumber(const int min, const int max)
 {
     // TODO: Return a random integer number between min and max
-    return 1;
+    return min + rand() % (max - min + 1);
 }
 
 vector<string> readWordListFromFile(const string& filePath)
@@ -51,7 +51,9 @@ vector<string> readWordListFromFile(const string& filePath)
 bool isCharInWord(const char ch, const string& word)
 {
     // TODO: return true if ch is in word else return false
-    return true;
+    for(char character: word)
+        if(character == ch) return true;
+    return false;
 }
 
 /***
@@ -65,7 +67,8 @@ string chooseWordFromList(const vector<string>& wordList, int index)
 {
     // TODO: Return a lowercase word in the index position of the vector wordList.
     string answer;
-
+    for(char ch: wordList[index])
+        answer += tolower(ch);
     return answer;
 }
 
@@ -77,8 +80,7 @@ string chooseWordFromList(const vector<string>& wordList, int index)
 ***/
 string generateHiddenCharacters(string answerWord){
     // TODO: Based on answerWord's length, generate hidden characters in form of "---"
-    string secretWord;
-
+    string secretWord = string(answerWord.size(), '-'); 
     return secretWord;
 }
 
@@ -99,6 +101,8 @@ char getInputCharacter() {
 void updateSecretWord(string& secretWord, const char ch, const string& word)
 {
     // TODO: Update the secret word if the character ch is in the answer word.
+    for(int i = 0; i < (int)word.size(); i ++)
+        if(ch == word[i]) secretWord[i] = ch;
 }
 
 /***
@@ -110,6 +114,7 @@ void updateSecretWord(string& secretWord, const char ch, const string& word)
 ***/
 void updateEnteredChars(const char ch, string& chars){
     // TODO: append the character ch is in end of the text chars
+    chars += ch;
 }
 
 /***
@@ -120,6 +125,7 @@ void updateEnteredChars(const char ch, string& chars){
 ***/
 void updateIncorrectGuess(int& incorrectGuess){
     // TODO: increase the value of incorrectGuess by 1
+    incorrectGuess++;
 }
 
 /***
@@ -146,5 +152,15 @@ void processData(const char ch, const string& word,
             update incorrectGuess: call updateIncorrectGuess() function
             update incorrectChars: call updateEnteredChars() function
     ***/
+   if(isCharInWord(ch, word))
+   {
+        updateSecretWord(secretWord, ch, word);
+        updateEnteredChars(ch, correctChars);
+   }
+   else
+   {
+        updateIncorrectGuess(incorrectGuess);
+        updateEnteredChars(ch, incorrectChars);
+   }
 }
 
--- AdvProg_L7-Snake/Game.cpp
+++ AdvProg_L7-Snake/Game.cpp
@@ -52,10 +52,31 @@ Game::~Game()
 
 void Game::snakeMoveTo(Position pos) {
 	//  START CODE HERE
-	//
-	//
-	//
-	//
+	switch (getCellType(pos))
+	{
+		case CELL_OFF_BOARD:
+		{
+			status = GAME_OVER;
+			break;
+		}
+		case CELL_SNAKE:
+		{
+			status = GAME_OVER;
+			break;
+		}
+		case CELL_CHERRY:
+		{
+			score++;
+			snake.eatCherry();
+			addCherry();
+			break;
+		}
+		default:
+		{
+			setCellType(pos, CELL_SNAKE);
+			break;
+		}
+	}
 	// END CODE HERE
 }
 
@@ -74,9 +95,7 @@ void Game::snakeLeave(Position position)
 {
 	// Suggestion: use setCellType() method in Game class
 	// START CODE HERE
-	//  
-	//
-	//
+	setCellType(position, CELL_EMPTY);
 	// END CODE HERE
 }
 
@@ -104,8 +123,8 @@ void Game::processUserInput(Direction direction)
  ***/
 bool Game::canChange(Direction current, Direction next) const {
 	if (current == UP || current == DOWN) 
-		return 0; // YOUR CODE HERE
-	return 0;// YOUR CODE HERE
+		return (next != UP && next != DOWN);
+	return (next != LEFT && next != RIGHT);
 }
 
 
@@ -128,14 +147,15 @@ void Game::nextStep()
 {
 	while (!inputQueue.empty()) {
 		// get the input direction from input queue
-        Direction next ; // YOUR CODE HERE
+        Direction next = inputQueue.front();
 
 		// remove the front of input queue
         // YOUR CODE HERE
+		inputQueue.pop();
 
 		// check if snake can move to the next direction, set current direction as next
         if (canChange(currentDirection, next)) {
-        	// YOUR CODE HERE
+			currentDirection = next;
         	break;
 		}
     }
@@ -162,15 +182,14 @@ void Game::addCherry()
 		// init a random position inside the play screen (width, height)
 		// Suggestion: use rand() function
 
-        Position randomPos; // YOUR CODE HERE
+        Position randomPos = {rand() % width, rand() % height};
 		
 		// check if the randomPos is EMPTY 
         if (getCellType(randomPos) == CELL_EMPTY) {
 
         	// assign the cherry position as randomPos, and set randomPos type as CELL_CHERRY
-
-			// YOUR CODE HERE
-			// YOUR CODE HERE
+			cherryPosition = randomPos;
+			setCellType(randomPos, CELL_CHERRY);
 
        		break;
         }
@@ -197,7 +216,11 @@ void Game::setCellType(Position pos, CellType cellType)
 	// Suggestion: use pos.isInsideBox(...) in Position class
 	//
 	// START CODE HERE
-	//  
+	if(pos.isInsideBox(0, 0, width, height))
+	{
+		squares[pos.y][pos.x] = cellType;
+	}
+
 	// END CODE HERE
 }
 
